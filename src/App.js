fec28516cef8d699ce66e7906d2464390cb8b17d
AhmedElakeed <ahmed.elakeed@advintic.com>
2022-09-24 16:08:11 +0200

    com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    const addUserHandler = (username, age) => {
+        setUsers((prevState) => {
+            return [...prevState, {username: username, age: age,id:Math.random().toString()}]
+        })
+    }
+
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}/>
+            <UserList users={users}/>
+        </div>
+    );
 }
 
 export default App;
